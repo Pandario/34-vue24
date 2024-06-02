5bcd1aca95412469e57b3aefb4218e420a07a473
Andrew <dinogalaktika@gmail.com>
2024-06-02 17:47:21 +0200

    minor cosmetic last  changes

--- src/components/NewsList.tsx
+++ src/components/NewsList.tsx
@@ -13,6 +13,7 @@ export default defineComponent({
     const newsStore = useNewsStore()
     const pageViewStore = usePageViewStore()
     const searchQuery = ref('')
+    const isScrolled = ref(false)
 
     const loadMoreArticles = () => {
       const isMediumOrLarger = window.matchMedia('(min-width: 768px)').matches
@@ -28,13 +29,19 @@ export default defineComponent({
       pageViewStore.resetArticlesDisplayed()
     }
 
+    const handleScroll = () => {
+      isScrolled.value = window.scrollY > 0
+    }
+
     onMounted(() => {
       newsStore.fetchNews()
       window.addEventListener('scroll', loadMoreArticles)
+      window.addEventListener('scroll', handleScroll)
     })
 
     onUnmounted(() => {
       window.removeEventListener('scroll', loadMoreArticles)
+      window.removeEventListener('scroll', handleScroll)
     })
 
     const displayedArticles = computed(() => {
@@ -51,12 +58,13 @@ export default defineComponent({
       handleSearch,
       displayedArticles,
       newsStore,
+      isScrolled
     }
   },
   render() {
     return (
       <div>
-        <div class="flex justify-center my-4">
+        <div class={`sticky top-4 flex justify-center my-4 ${this.isScrolled ? 'opacity-75' : ''}`}>
           <SearchBar onSearch={this.handleSearch} />
         </div>
         <div class="flex flex-wrap justify-center">
@@ -66,22 +74,30 @@ export default defineComponent({
             <div class="text-red-500 text-center">{this.newsStore.error}</div>
           ) : (
             <>
-              {this.displayedArticles.map(article => (
-                <div key={article.url} class="m-4 p-4 border rounded shadow-md flex flex-col justify-between bg-white w-80 h-auto">
-                  {article.urlToImage && (
-                    <div class='h-40 mb-4'>
-                      <img src={article.urlToImage} alt={article.title} class='object-cover w-full h-full rounded-t' />
-                    </div>
-                  )}
-                  <div class="flex-grow flex flex-col">
-                    <h2 class="text-xl font-semibold mb-2">{article.title}</h2>
-                    <p class='text-sm text-gray-600'>{article.formattedDate}</p>
-                  </div>
-                  <div class='mt-4'>
-                    <a href={article.url} target="_blank" class='block w-full p-3 bg-slate-500 hover:bg-slate-600 text-white text-center rounded'>Read more</a>
+              {this.displayedArticles.length === 0 ? (
+                <div class="m-4 p-4 border rounded shadow-md flex flex-col justify-between bg-white w-80 h-auto">
+                  <div class="flex-grow flex flex-col justify-center items-center">
+                    <h2 class="text-xl font-semibold mb-2">No news found :(</h2>
                   </div>
                 </div>
-              ))}
+              ) : (
+                this.displayedArticles.map(article => (
+                  <div key={article.url} class="m-4 p-4 border rounded shadow-md flex flex-col justify-between bg-white w-80 h-auto">
+                    {article.urlToImage && (
+                      <div class='h-40 mb-4'>
+                        <img src={article.urlToImage} alt={article.title} class='object-cover w-full h-full rounded-t' />
+                      </div>
+                    )}
+                    <div class="flex-grow flex flex-col">
+                      <h2 class="text-xl font-semibold mb-2">{article.title}</h2>
+                      <p class='text-sm text-gray-600'>{article.formattedDate}</p>
+                    </div>
+                    <div class='mt-4'>
+                      <a href={article.url} target="_blank" class='block w-full p-3 bg-slate-500 hover:bg-slate-600 text-white text-center rounded'>Read more</a>
+                    </div>
+                  </div>
+                ))
+              )}
             </>
           )}
         </div>
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -27,13 +27,13 @@ export default defineComponent({
       <div class="mb-4 w-full max-w-screen-lg flex justify-center">
         <input 
           v-model={this.query} 
-          class="p-2 border border-gray-300 rounded w-3/4" 
+          class="p-2 border border-gray-300 rounded w-3/4 focus:placeholder-transparent" 
           type="text" 
           placeholder="Search..." 
           onKeyup={this.handleKeyup}
         />
         <button 
-          class="p-2 ml-2 bg-blue-500 text-white rounded" 
+          class="p-2 ml-2 bg-blue-500 text-white rounded hover:bg-blue-700" 
           onClick={this.emitSearch}
         >
           Search
